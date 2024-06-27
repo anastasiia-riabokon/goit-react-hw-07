0041af90631ad9496ca2f3034b63cb4090a3bcbc
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-06-27 19:13:58 +0300

    feat: loading and error

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,14 +4,17 @@ import ContactForm from "./ContactForm/ContactForm";
 import Notification from "./Notification/Notification";
 import css from "./App.module.css";
 import {useDispatch, useSelector} from "react-redux";
-import {selectContacts} from "../redux/contactsSlice";
+import {selectContacts, selectError, selectLoading} from "../redux/contactsSlice";
 import {selectNameFilter} from "../redux/filtersSlice";
 import {useEffect} from "react";
 import {fetchContacts} from "../redux/operations";
+import {DNA} from "react-loader-spinner";
 
 function App() {
   const contacts = useSelector(selectContacts);
   const filter = useSelector(selectNameFilter);
+  const isLoading = useSelector(selectLoading);
+  const isError = useSelector(selectError);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -27,11 +30,15 @@ function App() {
       <h1 className={css.title}>Phonebook</h1>
       <ContactForm />
       <SearchBox />
-      {filteredContacts.length !== 0 ? (
+      {isLoading && <DNA wrapperStyle={{position: "absolute", top: "30%", right: "30%"}} />}
+      {!isLoading && !isError && filteredContacts.length !== 0 && (
         <ContactList users={filteredContacts} />
-      ) : (
-        <Notification text={`No contact found with the name ${filter}`} />
       )}
+      {filteredContacts.length === 0 && contacts.length !== 0 && (
+        <Notification text={`No contact found ${filter}`} />
+      )}
+
+      {isError && <Notification text={"Woops! Something went wrong😰"} />}
     </div>
   );
 }
